4a247fc5542b6193d6e8434eec850fe98b36aabf
tmolonushenko <tmolonushenko@gmail.com>
2022-05-16 02:59:36 +0300

    added tests

--- src/test/java/cloud/autotests/config/kinopoisk/App.java
+++
@@ -1,7 +0,0 @@
-package cloud.autotests.config.kinopoisk;
-
-import org.aeonbits.owner.ConfigFactory;
-
-public class App {
-    public static AppConfig config = ConfigFactory.create(AppConfig.class, System.getProperties());
-}
